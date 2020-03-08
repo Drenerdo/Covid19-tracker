0ee80bd6d236319c769602f255a6d46cdaec9dbd
Deepbit Labs <accounts@deepbitlabs.io>
2020-03-08 04:14:42 -0400

    Making changes to displaying data

--- Assets/Scripts/DataManager.cs
+++ Assets/Scripts/DataManager.cs
@@ -3,53 +3,192 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using System.Linq;
+using System.Linq.Expressions;
+using UnityEngine.UI;
 
 public class DataManager : MonoBehaviour
 {
-    private static DataManager _instance;
-
-    public static DataManager Instance
+    void Start()
     {
-        get
-        {
-            if (_instance == null)
-            {
-                _instance = FindObjectOfType<DataManager>();
-                if (_instance == null)
-                {
-                    GameObject go = new GameObject();
-                    go.name = typeof(DataManager).Name;
-                    _instance = go.AddComponent<DataManager>();
-                    DontDestroyOnLoad(go);
-                }
-            }
-            return _instance;
-        }
+        StartCoroutine(getCurrent());
+        StartCoroutine(getTotal());
+        StartCoroutine(getRecovered());
+        StartCoroutine(getCountries());
+
+        StartCoroutine(getImage());
+
     }
 
-    public IEnumerator Get(string url, object getData)
+    IEnumerator getImage()
     {
-        using(UnityWebRequest www = UnityWebRequest.Get(url))
-        {
-            yield return www.SendWebRequest();
+        //UnityWebRequest www = UnityWebRequest.GetTexture("https://covid2019-api.herokuapp.com/total");
+        //Debug.Log("Start");
 
-            if(www.isNetworkError)
+        //yield return www;
+
+        //Debug.Log("Loaded");
+        //Texture2D texture = www.texture;
+        ////this.gameObject.GetComponent<>().material.SetTexture( 0,texture );
+        //Image img = this.gameObject.GetComponent<Image>();
+        //img.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero);
+
+        using (UnityWebRequest uwr = UnityWebRequestTexture.GetTexture("https://flagpedia.net/download/country-codes-case-upper.json"))
+        {
+            yield return uwr.SendWebRequest();
+
+            if (uwr.isNetworkError || uwr.isHttpError)
             {
-                Debug.Log(www.error);
+                Debug.Log(uwr.error);
             }
             else
             {
-                if (www.isDone)
-                {
-                    string jsonResult = System.Text.Encoding.UTF8.GetString(www.downloadHandler.data);
+                // Get downloaded asset bundle
+                var texture = DownloadHandlerTexture.GetContent(uwr);
 
-                    DataOverview dataOverview = JsonUtility.FromJson<DataOverview>(jsonResult);
-
-                    //Debug.Log(dataOverview.confirmed);
-
-                    Debug.Log(jsonResult);
-                }
+                Debug.Log(texture);
             }
+        }
+    }
+
+    IEnumerator getTotal()
+    {
+        UnityWebRequest www = UnityWebRequest.Get("https://covid2019-api.herokuapp.com/total");
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+
+
+            byte[] results = www.downloadHandler.data;
+
+            if (www.isDone)
+            {
+                string jsonResult = System.Text.Encoding.UTF8.GetString(www.downloadHandler.data);
+                Debug.Log(jsonResult);
+
+                TotalData totalData = JsonUtility.FromJson<TotalData>(jsonResult);
+
+                Debug.Log(" Here are the confirmed cases " + totalData.confirmed);
+                Debug.Log(" Here are the recovered cases " + totalData.recovered);
+                Debug.Log(" Here are the casualities " + totalData.deaths);
+
+                //DataTypeVisualizer dataType = JsonUtility.FromJson<DataTypeVisualizer>(jsonResult);
+            }
+        }
+    }
+
+    IEnumerator getCurrent()
+    {
+        UnityWebRequest www = UnityWebRequest.Get("https://covid2019-api.herokuapp.com/current");
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+
+
+            byte[] results = www.downloadHandler.data;
+
+            if (www.isDone)
+            {
+                string jsonResult = System.Text.Encoding.UTF8.GetString(www.downloadHandler.data);
+                Debug.Log(jsonResult);
+
+                DataTypeVisualizer dataType = JsonUtility.FromJson<DataTypeVisualizer>(jsonResult);
+                Debug.Log("Getting current data!");
+            }
+        }
+    }
+
+    IEnumerator getRecovered()
+    {
+        UnityWebRequest www = UnityWebRequest.Get("https://covid2019-api.herokuapp.com/recovered");
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            byte[] results = www.downloadHandler.data;
+
+            if (www.isDone)
+            {
+                string jsonResult = System.Text.Encoding.UTF8.GetString(www.downloadHandler.data);
+                Debug.Log(jsonResult);
+
+                DataTypeVisualizer dataType = JsonUtility.FromJson<DataTypeVisualizer>(jsonResult);
+
+                //Debug.Log(dataType.recovered);
+                //Debug.Log(dataType.dt);
+                //Debug.Log(dataType.ts);
+            }
+        }
+    }
+
+    IEnumerator getCountries()
+    {
+        UnityWebRequest www = UnityWebRequest.Get("https://covid2019-api.herokuapp.com/countries");
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            byte[] results = www.downloadHandler.data;
+
+            if (www.isDone)
+            {
+                string jsonResult = System.Text.Encoding.UTF8.GetString(www.downloadHandler.data);
+                Debug.Log(jsonResult);
+
+                //DataTypeVisualizer dataType = JsonUtility.FromJson<DataTypeVisualizer>(jsonResult);
+
+                //Debug.Log(dataType.recovered);
+                //Debug.Log(dataType.dt);
+                //Debug.Log(dataType.ts);
+            }
+        }
+    }
+
+    IEnumerator getconfirmed()
+    {
+        UnityWebRequest www = UnityWebRequest.Get("https://covid2019-api.herokuapp.com/deaths");
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+
+
+            byte[] results = www.downloadHandler.data;
+
+            if (www.isDone)
+            {
+                string jsonResult = System.Text.Encoding.UTF8.GetString(www.downloadHandler.data);
+                Debug.Log(jsonResult);
+
+                //DataTypeVisualizer dataType = JsonUtility.FromJson<DataTypeVisualizer>(jsonResult);
+
+                //Debug.Log(dataType.recovered);
+                //Debug.Log(dataType.dt);
+                //Debug.Log(dataType.ts);
+            }
+
         }
     }
 }
---
+++ Assets/Scripts/TotalData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+
+[Serializable]
+public class TotalData
+{
+    public int confirmed { get; set; }
+    public int deaths { get; set; }
+    public int recovered { get; set; }
+    public string dt { get; set; }
+    public double ts { get; set; }
+}
